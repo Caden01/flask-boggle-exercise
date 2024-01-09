f48d28c6a7a071daa3c0f9a95452fe3a9f213faa
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2024-01-09 10:25:12 -0800

    Show boggle game board and form

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask, session, render_template, request, jsonify
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "SECRET"
+
 boggle_game = Boggle()
+
+@app.route("/")
+def home():
+    board = boggle_game.make_board()
+
+    return render_template("index.html", board=board)
