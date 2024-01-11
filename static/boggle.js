84a386adbdd7e23b03745c36d172fca1b0ed38dc
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2024-01-11 07:14:51 -0800

    Add messages that tells user if word is valid and keeps user score

--- static/boggle.js
+++ static/boggle.js
@@ -2,6 +2,7 @@ const $form = $("form");
 const $input = $("input");
 const $message = $(".message");
 
+let score = 0;
 async function handleForm(evt) {
   evt.preventDefault();
   let word = $input.val();
@@ -9,7 +10,16 @@ async function handleForm(evt) {
   const res = await axios.get("/check-word", {
     params: { word: word },
   });
-  console.log(res);
+
+  if (res.data.result === "not-on-board") {
+    $message.text(`${word} is not on this board`);
+  } else if (res.data.result === "not-word") {
+    $message.text(`${word} is not a valid word`);
+  } else {
+    $message.text(`You found the word "${word}" on the board`);
+    score += word.length;
+    console.log(score);
+  }
 }
 
 $form.on("submit", handleForm);
